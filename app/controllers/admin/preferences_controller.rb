8d79f95e4969c0b064035a6130a0630efb23ccf7
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-04-17 11:06:59 -0700

    Done.

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,6 +1,5 @@
 class Admin::PreferencesController < ActionController::Base
   def index
-    @preference = Preference.first_or_create(allow_create_artists: true, allow_create_songs: true, song_sort_order: "DESC", artist_sort_order: "DESC")
   end
 
   private
